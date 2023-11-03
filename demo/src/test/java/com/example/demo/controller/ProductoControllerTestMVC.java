6665bcfbbe9d33852b68dc1c8c2f4f19eee5a003
Natalia Brenda Andino <nandinoatencio@gmail.com>
2023-11-03 00:38:33 -0300

    comentarios con lo que resta hacer de testeo (actividad práctica)

--- demo/src/test/java/com/example/demo/controller/ProductoControllerTestMVC.java
+++ demo/src/test/java/com/example/demo/controller/ProductoControllerTestMVC.java
@@ -1,9 +1,5 @@
 package com.example.demo.controller;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,18 +10,14 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import com.example.demo.entity.Producto;
-import com.example.demo.response.ModelResponse;
 import com.example.demo.service.service_implementa.ProductoService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -55,31 +47,16 @@ public class ProductoControllerTestMVC {
 
     @Test
     public void testListarProductosEndpoint() throws Exception {
-        
-        when(productoService.mostrarProducto()).thenReturn(productos);
-        ResponseEntity<?> responseEntity = productoController.listarProductos();
-        assertThat(responseEntity.getBody()).isEqualTo(productos);
-        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
 
         this.mockMvc.perform(MockMvcRequestBuilders.get("/producto"))
                 .andExpect(MockMvcResultMatchers.status().isOk());
-                //para el fallido, podemos seguir poniendo .andExpected
     }
 
-    // completar el fallido de testListarProductosEndpoint con la pista anterior
-    //// completar: buscarProductoXId, buscarProductoXNombre
+    // completar: buscarProductoXId, buscarProductoXNombre
     // tanto caso exitoso como fallido
 
     @Test
     void testGuardarProductoEndpoint() throws Exception {
-
-        // Crear un ModelResponse
-        ModelResponse modelResponse = new ModelResponse("Operación exitosa", "Success");
-
-        // Configurar el servicio para que devuelva un ResponseEntity con
-        // HttpStatus.CREATED
-        when(productoService.guardarProducto(producto))
-                .thenReturn(modelResponse);
 
         this.mockMvc.perform(MockMvcRequestBuilders.post("/producto")
                 .contentType(MediaType.APPLICATION_JSON)
@@ -92,12 +69,8 @@ public class ProductoControllerTestMVC {
     @Test
     void testActualizarProductoEndpoint() throws Exception {
         Long id = 1L;
-        // Configurar el servicio para que devuelva el Producto modificado
-        when(productoService.actualizarProducto(id, producto))
-                .thenReturn(producto);
 
-        // Realizar la solicitud PUT
-        mockMvc.perform(put("/producto/{id}", id)
+        this.mockMvc.perform(put("/producto/{id}", id)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(new ObjectMapper().writeValueAsString(producto)))
                 .andExpect(MockMvcResultMatchers.status().isOk());
